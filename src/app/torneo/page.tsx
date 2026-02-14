3acec9b158ebe4d2af93544d5eea31a413375e1c
marcocipriani <marcocipriani.dev@gmail.com>
2026-02-14 16:31:54 +0100

    separate tournament news

--- src/app/torneo/page.tsx
+++ src/app/torneo/page.tsx
@@ -17,18 +17,7 @@ import ClassificaPage from '../classifica/page'
 import { EventDialog } from '@/components/EventDialog'
 import { toast } from "sonner" 
 
-const COMUNICATI = [
-    { id: 9, data: "10/01/2026", titolo: "Comunicato n. 9", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-9-10-01-2026_lD6l.pdf" },
-    { id: 8, data: "06/12/2025", titolo: "Comunicato n. 8", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-8-06-12-2025_sn25.pdf" },
-    { id: 7, data: "22/11/2025", titolo: "Comunicato n. 7", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-7-22-11-2025_4EZ3.pdf" },
-    { id: 6, data: "08/11/2025", titolo: "Comunicato n. 6", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-6-08-11-2025_OMop.pdf" },
-    { id: 99, data: "02/11/2025", titolo: "Comunicazione Importante Società", url: "https://cdn.enjore.com/source/doc/comunicate/113994-comunicazione-importante-alle-societa-partecipanti-ai-campionati-asi-del-02112025_S455.pdf" },
-    { id: 4, data: "25/10/2025", titolo: "Comunicato n. 4", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-4-25-10-2025_FT7L.pdf" },
-    { id: 102, data: "14/10/2025", titolo: "CIRCOLARE n. 2", url: "https://cdn.enjore.com/source/doc/comunicate/113994-circolare-n-2-del-14102025_tIZN.pdf" },
-    { id: 2, data: "11/10/2025", titolo: "Comunicato n. 2", url: "https://cdn.enjore.com/source/doc/comunicate/113994-campionato-asi-over35_artimestieri_20252026-comunicato-2-11-10-2025_ifIS.pdf" },
-    { id: 101, data: "06/10/2025", titolo: "CIRCOLARE n. 1", url: "https://cdn.enjore.com/source/doc/comunicate/113994-circolare-n-1-del-06102025_84fG.pdf" },
-    { id: 1, data: "01/10/2025", titolo: "Regolamento", url: "https://cdn.enjore.com/source/doc/tournament_doc/113994-zFUMr1Pcin-regolamento.pdf" },
-];
+import { COMUNICATI } from '@/lib/comunicati'
 
 export default function TorneoPage() {
   const router = useRouter()

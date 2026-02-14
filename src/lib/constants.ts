1730bd0ab2bf9d013a367c2640face8283b1d419
marcocipriani <marcocipriani.dev@gmail.com>
2026-02-14 16:59:50 +0100

    add header and declutter

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,3 +24,31 @@ export const BOMBER_TAGS = [
     "Finto infortunato",
     "Vado a farmi la doccia"
 ];
+
+export const FORMATIONS: any = {
+  "4-4-2": [
+    { id: 'POR', role: 'PT', top: '88%', left: '50%' },
+    { id: 'TS', role: 'DIF', top: '70%', left: '15%' }, { id: 'DC1', role: 'DIF', top: '70%', left: '38%' }, { id: 'DC2', role: 'DIF', top: '70%', left: '62%' }, { id: 'TD', role: 'DIF', top: '70%', left: '85%' },
+    { id: 'ES', role: 'CEN', top: '45%', left: '15%' }, { id: 'CC1', role: 'CEN', top: '45%', left: '38%' }, { id: 'CC2', role: 'CEN', top: '45%', left: '62%' }, { id: 'ED', role: 'CEN', top: '45%', left: '85%' },
+    { id: 'ATT1', role: 'ATT', top: '15%', left: '35%' }, { id: 'ATT2', role: 'ATT', top: '15%', left: '65%' }
+  ],
+  "4-3-3": [
+    { id: 'POR', role: 'PT', top: '88%', left: '50%' },
+    { id: 'TS', role: 'DIF', top: '70%', left: '15%' }, { id: 'DC1', role: 'DIF', top: '70%', left: '38%' }, { id: 'DC2', role: 'DIF', top: '70%', left: '62%' }, { id: 'TD', role: 'DIF', top: '70%', left: '85%' },
+    { id: 'CC', role: 'CEN', top: '50%', left: '50%' }, { id: 'MZ1', role: 'CEN', top: '40%', left: '30%' }, { id: 'MZ2', role: 'CEN', top: '40%', left: '70%' },
+    { id: 'AS', role: 'ATT', top: '20%', left: '20%' }, { id: 'ATT', role: 'ATT', top: '15%', left: '50%' }, { id: 'AD', role: 'ATT', top: '20%', left: '80%' }
+  ],
+  "3-5-2": [
+    { id: 'POR', role: 'PT', top: '88%', left: '50%' },
+    { id: 'DC1', role: 'DIF', top: '75%', left: '30%' }, { id: 'DC2', role: 'DIF', top: '75%', left: '50%' }, { id: 'DC3', role: 'DIF', top: '75%', left: '70%' },
+    { id: 'MED', role: 'CEN', top: '55%', left: '50%' }, { id: 'ES', role: 'CEN', top: '45%', left: '15%' }, { id: 'CC1', role: 'CEN', top: '45%', left: '35%' }, { id: 'CC2', role: 'CEN', top: '45%', left: '65%' }, { id: 'ED', role: 'CEN', top: '45%', left: '85%' },
+    { id: 'ATT1', role: 'ATT', top: '15%', left: '40%' }, { id: 'ATT2', role: 'ATT', top: '15%', left: '60%' }
+  ],
+  "4-2-3-1": [
+    { id: 'POR', role: 'PT', top: '90%', left: '50%' },
+    { id: 'TS', role: 'DIF', top: '75%', left: '15%' }, { id: 'DC1', role: 'DIF', top: '75%', left: '38%' }, { id: 'DC2', role: 'DIF', top: '75%', left: '62%' }, { id: 'TD', role: 'DIF', top: '75%', left: '85%' },
+    { id: 'MED1', role: 'CEN', top: '55%', left: '35%' }, { id: 'MED2', role: 'CEN', top: '55%', left: '65%' },
+    { id: 'TRQ1', role: 'CEN', top: '35%', left: '20%' }, { id: 'TRQ2', role: 'CEN', top: '35%', left: '50%' }, { id: 'TRQ3', role: 'CEN', top: '35%', left: '80%' },
+    { id: 'ATT', role: 'ATT', top: '15%', left: '50%' }
+  ]
+}
